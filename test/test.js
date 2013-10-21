f0b4b18fdb9816084524223cb95beaaad7d5b2b9
Flarnie Marchan <fnonemaker@gmail.com>
2013-10-20 17:47:07 -0700

    Add to game logic and write tests for Piece class.

--- test/test.js
+++ test/test.js
@@ -3,14 +3,33 @@ var Board = require("./board.js").Board;
 var Piece = require("./piece.js").Piece;
 var Game = require("./game.js").Game;
 
+describe("Piece", function(){
+	var darkPiece = new Piece("black");
+	var lightPiece = new Piece("white");
+	describe("#color", function(){
+	  it("should have the color assigned to it", function(){
+		  assert.equal(darkPiece.color, "black");
+		  assert.equal(lightPiece.color, "white");
+	  })
+	  it("should only allow black or white", function(){
+		  var makeBadPiece = function(){
+			  new Piece("foo");
+		  };
+		  assert.throw(makeBadPiece(), Error, "Color must be black or white");
+	  })
+	})
+	describe("#flip", function(){
+		it("should switch colors when flipped", function(){
+			darkPiece.flip();
+			assert.equal(darkPiece.color, "white");
+		})
+	});
+})
+
 describe("Board", function(){
 	
-});
-
-describe("Piece", function(){
-	
-});
+})
 
 describe("Game", function(){
 	
-});
+})
