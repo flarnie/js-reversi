2c0b6f2a35b5b57d03597bd314e8038756b86f4c
Flarnie Marchan <fnonemaker@gmail.com>
2013-10-20 19:04:40 -0700

    Add test for board class.

--- test/test.js
+++ test/test.js
@@ -27,6 +27,50 @@ describe("Piece", function(){
 })
 
 describe("Board", function(){
+	var testBoard = new Board();
+	describe("#grid", function(){
+		it("should be 8x8", function(){
+			assert.equal(testBoard.grid.length, 8);
+			for(i = 0; i < 8; i ++){
+				assert.equal(testBoard.grid[i].length, 8);
+			}
+		})
+		it("should begin with 4 pieces in the center", function(){
+			assert.equal(testBoard.grid[3][3].color, "white");
+			assert.equal(testBoard.grid[3][4].color, "black");
+			assert.equal(testBoard.grid[4][3].color, "black");
+			assert.equal(testBoard.grid[4][4].color, "white");
+		})
+		it("should start out empty except for the 4 center pieces", function(){
+			testBoard.grid.forEach( function(row, i){
+				row.forEach( function(spot, j){
+					if((i !== 3 && i !== 4) && (j !== 3 && j !== 4)){
+						assert.equal(spot, undefined);
+					}
+				});
+			});
+		})
+	}) // end describe #grid
+	
+	describe("putting pieces on board", function(){
+		
+	})
+	describe("#full", function(){
+		it("should not begin full", function(){
+			var anotherBoard = new Board();
+			assert.equal(anotherBoard.full, false);
+		})
+		it("should be full if no spots are empty", function(){
+			// Fill the board for the test
+			for(i = 0; i < 8; i ++){
+				for(j = 0; j < 8; j ++){
+					testBoard[i][j] = new Piece("white");
+				}
+			}
+			// Test fullness
+		  assert.equal(testBoard.full, true);
+		});
+	})
 	
 })
 
